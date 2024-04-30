bc9d7f937d97bc1b7359264b3493ce0a4643ff2a
User Nika  Azizova <nikaazizova64@gmail.com>
2024-04-30 16:00:50 +0400

    added img of enter, editing and remove to the table

--- src/Components/Editing/Editing.jsx
+++ src/Components/Editing/Editing.jsx
@@ -1,4 +1,6 @@
-import styles from './Editing.module.scss'
+import styles from './Editing.module.scss';
+import done from '../../assets/done.png';
+import remove from '../../assets/remove.png';
 
 export default function Editing(props) {
     const { word, transcription, translation } = props;
@@ -7,7 +9,7 @@ export default function Editing(props) {
             <td className={styles.td}><input className={styles.inputWord} type='text' placeholder={word} /></td>
             <td className={styles.td}><input className={styles.inputWord} type='text' placeholder={transcription} /></td>
             <td className={styles.td}><input className={styles.inputWord} type='text' placeholder={translation} /></td>
-            <td className={styles.td}><button>ред</button></td>
+            <td className={styles.td}><img className={styles.doneImg} src={done} alt="enter"/><img className={styles.removeImg} src={remove} alt="delete" /></td>
 
         </tr>
     )
--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,4 +1,6 @@
 import styles from './Table.module.scss'
+import pencil from '../../assets/pencil.png'
+import remove from '../../assets/remove.png'
 
 export default function Tables(props) {
     const { word, transcription, translation } = props;
@@ -7,7 +9,7 @@ export default function Tables(props) {
             <td className={styles.td}>{word}</td>
             <td className={styles.td}>{transcription}</td>
             <td className={styles.td}>{translation}</td>
-            <td className={styles.td}><button>ред</button><button>удал</button></td>
+            <td className={styles.td}><img className={styles.editingImg} src={pencil} alt="editing" /> <img className={styles.removeImg} src={remove} alt="remove" /></td>
         </tr>
     )
 }
