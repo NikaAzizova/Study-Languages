2972ad3d2c2a796fcc598be26d84b3e007d15ec0
User Nika  Azizova <nikaazizova64@gmail.com>
2024-05-22 15:56:09 +0400

    added logo img

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,26 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Button from "../Button/Button.jsx";
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.png'
 
 export default function Header() {
     return (
 
         <header className={styles.header}>
             <div className={styles.wrapper}>
-                 <Button>Войти</Button> 
+
+                <nav className={styles.nav}>
+                    <NavLink to='/' className={styles.navLink}><img className={styles.logo} src={logo} alt="logo" /></NavLink>
+                </nav>
             </div>
+
             <nav className={styles.nav}>
-            <NavLink to='/' className={styles.navLink}>Home</NavLink>
-            <NavLink to='/game' className={styles.navLink}>Game</NavLink>
-            <NavLink to='/table' className={styles.navLink}>Table</NavLink>
-          </nav>
+                <NavLink to='/' className={styles.navLink}>Home</NavLink>
+                <NavLink to='/game' className={styles.navLink}>Game</NavLink>
+                <NavLink to='/table' className={styles.navLink}>Table</NavLink>
+                <Button>Войти</Button>
+            </nav>
+
         </header>
 
 
