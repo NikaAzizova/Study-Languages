b6d5f6b0934654f02486c759228b9fe1ee8c44ff
User Nika  Azizova <nikaazizova64@gmail.com>
2024-08-06 14:13:42 +0400

    add changes

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,9 +1,17 @@
 import styles from './Table.module.scss'
 import AddingNewLine from '../AddingNewLine/AddingNewLine.jsx'
+import { useEffect, useState } from 'react';
+import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
+import { IoCheckmarkDoneCircle } from "react-icons/io5";
 import Row from "../Row/Row.jsx";
 
 
 export default function Table({ wordDictionary, setWordDictionary }) {
+
+    const [openEditing, setOpenEditing] = useState(false);
+    const [wordSt, setWordSt] = useState("");
+    const [transcriptionWord, setTranscription] = useState("");
+    const [translationWord, setTranslationWord] = useState("");
 
     //сохраняем отредактированные слова
     function handleSave({ id, wordSt, transcriptionWord, translationWord }) {
@@ -46,7 +54,77 @@ export default function Table({ wordDictionary, setWordDictionary }) {
                                 </thead>
                                 <tbody>
                                     {wordDictionary.map((item) => {
-                                        return <Row wordDictionary={item} key={item.id} handleSave={handleSave} handleRemove={handleRemove} />;
+                                        <tr className={styles.tr} >
+                                            <td className={styles.td}>
+                                                <span className={styles.error}>{wordError}</span> {/*Здесь появится инф-я об ошибке*/}
+                                                {openEditing ? (
+                                                    <input
+                                                        className={styles.inputWord}
+                                                        type="text"
+                                                        value={wordSt}
+                                                        onChange={(e) => setWordSt(e.target.value)}
+                                                    />
+                                                ) : (
+                                                    word
+                                                )}
+                                            </td>
+                                            <td className={styles.td}>
+                                                {openEditing ? (
+                                                    <input
+                                                        type="text"
+                                                        className={styles.inputWord}
+                                                        value={transcriptionWord}
+                                                        onChange={(e) => setTranscription(e.target.value)}
+                                                    />
+                                                ) : (
+                                                    transcription
+                                                )}
+                                            </td>
+                                            <td className={styles.td}>
+                                                <span className={styles.error}>{translationError}</span> {/*Здесь появится инф-я об ошибке*/}
+                                                {openEditing ? (
+                                                    <input
+                                                        type="text"
+                                                        className={styles.inputWord}
+                                                        value={translationWord}
+                                                        onChange={(e) => setTranslationWord(e.target.value)}
+                                                    />
+                                                ) : (
+                                                    translation
+                                                )}
+                                            </td>
+                                            <td className={styles.td}>
+                                                {openEditing ? (
+                                                    <button className={styles.btnSave}
+                                                        onClick={() => {
+                                                            setOpenEditing(false),
+                                                                handleSave({
+                                                                    id,
+                                                                    wordSt,
+                                                                    transcriptionWord,
+                                                                    translationWord
+                                                                })
+                                                        }
+                                                        }>
+                                                        <IoCheckmarkDoneCircle
+                                                            className={styles.doneImg}
+                                                        />
+                                                    </button>
+                                                ) : (
+                                                    <BsFillPencilFill
+                                                        onClick={() => handleEditing(id)}
+                                                        className={styles.editingImg} disabled
+                                                    />
+                                                )}
+
+                                                <BsFillTrashFill
+                                                    onClick={() => handleRemove(id)}
+                                                    className={styles.removeImg}
+                                                />
+                                            </td>
+                                        </tr>
+
+                                        //return <Row wordDictionary={item} key={item.id} handleSave={handleSave} handleRemove={handleRemove} />;
                                     })}
                                 </tbody>
                             </table>
